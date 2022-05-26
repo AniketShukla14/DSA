815054e86866c082b966854851f4c991ed20cdc9
AniketShukla14 <aniket14042001@gmail.com>
2022-05-26 14:55:58 +0530

    Java Pattern Code

---
+++ pattern3.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		int n=5;
+		int sp=n-1;
+		int st=1;
+		for(int i=0;i<n;i++)
+		{
+		    for(int j=1;j<=sp;j++)
+		    {
+		    System.out.print("\t");
+		    }
+		    for(int k=1;k<=st;k++)
+		    {
+		        System.out.print("*\t");
+		    }
+		sp--;
+		st++;
+		    System.out.println();
+		}
+	}
+}
