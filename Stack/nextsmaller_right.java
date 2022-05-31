e3bff657706002c2b0955f45ad02f03ff64c9eeb
AniketShukla14 <aniket14042001@gmail.com>
2022-05-31 10:59:02 +0530

    Stack Codes

---
+++ Stack/nextsmaller_right.java
@@ -0,0 +1,29 @@
+class gfg{
+    public static int[] nextgreater(int arr[],int n)
+    {
+        Stack<Integer> st=new Stack<>();
+        int a[]=new int[n];
+     for(int i=n-1;i>=0;i--)
+     {
+         if(st.size()==0)
+         st.push(-1);
+         else if(st.size()>0 && st.peek()<=arr[i])
+         {
+             a[i]=st.peek();
+         }
+         else if(st.size()>0 && st.peek()>arr[i])
+         {
+             while(st.size()>0 && st.peek()>arr[i])
+             {
+                 st.pop();
+                 if(st.size()==0)
+                 st.push(-1);
+                 else 
+                 a[i]=st.peek();
+             }
+         }
+         st.push(arr[i]);
+     }
+     return a;
+    }
+    }
